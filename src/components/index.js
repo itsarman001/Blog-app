465dd378129bd686d7375f932230f3182829a090
Arman <armanar6712@gmail.com>
2024-07-14 17:22:01 +0530

    added login, signup and auth_layout

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,21 @@
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
 import LogoutBtn from "./Header/LogoutBtn";
 import BrandName from "./BrandName";
+import Button from "./Button";
+import Input from "./Input";
+import PostCard from "./PostCard";
+import Select from "./Select";
 
-export { Header, Footer, Container, LogoutBtn, BrandName };
+export {
+  Container,
+  Footer,
+  Header,
+  LogoutBtn,
+  BrandName,
+  Button,
+  Input,
+  PostCard,
+  Select,
+};
